7bb7781350baea3ac253ae00e77214a83d9b63a9
heiso <heiso.android@gmail.com>
2014-07-03 23:38:27 +0200

    animation step 1

--- src/frontend/movableObjectClass.js
+++ src/frontend/movableObjectClass.js
@@ -8,16 +8,25 @@ var touchEndHandler = function(e) {
   e.preventDefault();
   this.shoot.direction = {
     x: Math.abs(this.startCoord.screenX - e.changedTouches[0].screenX),
-    y: Math.abs(this.startCoord.screenY - e.changedTouches[0].screenY),
+    y: Math.abs(this.startCoord.screenY - e.changedTouches[0].screenY)
   };
   this.shoot.distance = Math.sqrt(sqr(this.shoot.direction.x) + sqr(this.shoot.direction.y));
   this.shoot.vitesse = this.shoot.distance / ((new Date()).getTime() - this.touchTime);
-  doAction(this.shoot);
+  //doAction(this.shoot);
+  console.log(this.shoot);
+  move("goal", this.shoot);
 };
+
 
 var touchMoveHandler = function(e) {
   e.preventDefault();
-}
+};
+
+var move = function(id, coord) {
+  var ob = document.getElementById("goal");
+  ob.style.transition ="all 1s ease-in-out";
+  ob.style.webkitTransform = "translate(" + coord.direction.x +  "px, -" + coord.direction.y +  "px)";
+};
 
 var MovableObject = function(id) {
 
