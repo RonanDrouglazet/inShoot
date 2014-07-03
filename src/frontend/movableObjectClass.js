cf2881731a87dfcb158bfcfc16f2f76b3a28601d
heiso <heiso.android@gmail.com>
2014-07-04 00:27:30 +0200

    animation step 2

--- src/frontend/movableObjectClass.js
+++ src/frontend/movableObjectClass.js
@@ -7,14 +7,14 @@ var touchStartHandler = function(e) {
 var touchEndHandler = function(e) {
   e.preventDefault();
   this.shoot.direction = {
-    x: Math.abs(this.startCoord.screenX - e.changedTouches[0].screenX),
-    y: Math.abs(this.startCoord.screenY - e.changedTouches[0].screenY)
+    x: Math.abs(e.changedTouches[0].clientX),
+    y: Math.abs(e.changedTouches[0].clientY)
   };
   this.shoot.distance = Math.sqrt(sqr(this.shoot.direction.x) + sqr(this.shoot.direction.y));
   this.shoot.vitesse = this.shoot.distance / ((new Date()).getTime() - this.touchTime);
   //doAction(this.shoot);
   console.log(this.shoot);
-  move("goal", this.shoot);
+  move(this);
 };
 
 
@@ -22,21 +22,25 @@ var touchMoveHandler = function(e) {
   e.preventDefault();
 };
 
-var move = function(id, coord) {
-  var ob = document.getElementById("goal");
-  ob.style.transition ="all 1s ease-in-out";
-  ob.style.webkitTransform = "translate(" + coord.direction.x +  "px, -" + coord.direction.y +  "px)";
+var move = function(movableObject) {
+
+  movableObject.ob.style.transition ="all 1s ease-in-out";
+  var movement = {
+    x : movableObject.shoot.direction.x - movableObject.ob.offsetLeft - (movableObject.ob.offsetWidth / 2),
+    y : movableObject.shoot.direction.y - movableObject.ob.offsetTop - (movableObject.ob.offsetHeight / 2)
+  }
+  movableObject.ob.style.webkitTransform = "translate(" + movement.x +  "px, " + movement.y +  "px)";
 };
 
 var MovableObject = function(id) {
 
-  var ob = document.getElementById(id);
+  this.ob = document.getElementById(id);
 
   this.startCoord = {};
   this.touchTime = null;
-  ob.addEventListener("touchstart", touchStartHandler.bind(this), false);
-  ob.addEventListener("touchmove", touchMoveHandler.bind(this), false);
-  ob.addEventListener("touchend", touchEndHandler.bind(this), false);
+  this.ob.addEventListener("touchstart", touchStartHandler.bind(this), false);
+  this.ob.addEventListener("touchmove", touchMoveHandler.bind(this), false);
+  this.ob.addEventListener("touchend", touchEndHandler.bind(this), false);
 
   this.shoot = {
     vitesse: 0,
